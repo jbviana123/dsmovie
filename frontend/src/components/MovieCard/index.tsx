98d281d7d870ca43688cd422fba15a10216e11f7
jbvianna123 <joaobvianna@hotmail.com>
2022-01-16 00:29:35 -0300

    show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -16,8 +16,8 @@ import { Movie } from "types/movie";
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore />
-                <Link to={`/form/1${movie.id}`}>
+                <MovieScore count={movie.count} score={movie.score}/>
+                <Link to={`/form/${movie.id}`}>
                 <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
             </div>
